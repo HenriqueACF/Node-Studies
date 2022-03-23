6f2c29bd48b0ca338bee186b7b43f688fa7f1228
henriqueacf <henrique.assis145@gmail.com>
2022-03-23 09:30:33 -0300

    feat: criado um middlleware para tratamento de excessoes

--- Valoriza/src/server.ts
+++ Valoriza/src/server.ts
@@ -1,8 +1,9 @@
 import "reflect-metadata";
-import express from "express";
-
+import express, {Request, Response, NextFunction} from "express";
+import "express-async-errors"
+//Routes
 import { router } from "./routes";
-
+//DB
 import "./database";
 
 const app = express();
@@ -11,4 +12,17 @@ app.use(express.json());
 
 app.use(router);
 
+//Middlleware
+app.use((err:Error, req:Request,res:Response, next:NextFunction)=>{
+    if(err instanceof Error){
+        return res.status(400).json({
+            error:err.message
+        })
+    }
+    return res.status(500).json({
+        status:'error',
+        message:'Internal Server Error'
+    })
+})
+//Port
 app.listen(3000, () => console.log("Server is running"));
