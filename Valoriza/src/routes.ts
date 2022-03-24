c78116813ef39cbbb8060df052cf9eafd27d87ad
henriqueacf <henrique.assis145@gmail.com>
2022-03-24 18:10:05 -0300

    feat [Users/Compliments/Tags]: criado um metodo para listagem dos users, compliments e tags

--- Valoriza/src/routes.ts
+++ Valoriza/src/routes.ts
@@ -5,6 +5,10 @@ import {ensureAdmin} from "./middlleware/ensureAdmin";
 import {AuthenticateUserController} from "./controllers/AuthenticateUserController";
 import {CreateComplimentController} from "./controllers/CreateComplimentController";
 import {ensureAuthenticate} from "./middlleware/ensureAuthenticate";
+import {ListUserSendComplimentsController} from "./controllers/ListUserSenderComplimentsController";
+import {ListUserReceiveComplimentsController} from "./controllers/ListUserReciverComplimentsController";
+import {ListTagsController} from "./controllers/ListTagsController";
+import {ListUsersController} from "./controllers/ListUsersController";
 
 
 const router = Router();
@@ -13,10 +17,20 @@ const createUserController = new CreateUserController();
 const createTagController = new CreateTagController()
 const authenticateController = new AuthenticateUserController()
 const createComplimentController = new CreateComplimentController()
+const userListComplimentsSend = new ListUserSendComplimentsController()
+const userListComplimentRecive = new ListUserReceiveComplimentsController()
+const listTags = new ListTagsController()
+const listUsers = new ListUsersController()
 
+//POST
 router.post('/tags', ensureAuthenticate, ensureAdmin, createTagController.handle)
 router.post("/users", createUserController.handle);
 router.post('/login', authenticateController.handle)
 router.post('/compliments', ensureAuthenticate, createComplimentController.handle)
+//GET
+router.get('/users/compliments/send', ensureAuthenticate, userListComplimentsSend.handle)
+router.get('/users/compliments/recived', ensureAuthenticate, userListComplimentRecive.handle)
+router.get('/tags', ensureAuthenticate, listTags.handle)
+router.get('/users', ensureAuthenticate, listUsers.handle)
 
 export { router };
---
+++ Valoriza/src/services/ListUserSendComplimentsService.ts
@@ -0,0 +1,16 @@
+import { getCustomRepository } from "typeorm";
+import {ComplimentsRepository} from "../repositories/ComplimentsRepository";
+
+export class ListUserSendComplimentsService {
+    async execute(user_id: string) {
+        const complimentsRepositories = getCustomRepository(ComplimentsRepository);
+
+        const compliments = await complimentsRepositories.find({
+            where: {
+                user_sender: user_id,
+            },
+        });
+
+        return compliments;
+    }
+}
