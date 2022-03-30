371a9ec3914ee0660d0936eb3843d1be0d6cfb85
henriqueacf <henrique.assis145@gmail.com>
2022-03-30 17:29:59 -0300

    feat [User]: estrutura de usuario criada

--- NPS/src/server.ts
+++ NPS/src/server.ts
@@ -1,13 +1,11 @@
+import 'reflect-metadata'
 import express, {Request, Response} from 'express'
+import './database'
+import {router} from "../routes";
 
 const app = express()
 
-app.get('/',(req: Request, res: Response) => {
-    return res.send('teste get')
-})
-
-app.post('/',(req: Request, res: Response) => {
-    return res.json({message: 'Dados salvos com sucesso'})
-})
+app.use(express.json())
+app.use(router)
 
 app.listen(3000, () => console.log('Server is Running on PORT:3000!'))
