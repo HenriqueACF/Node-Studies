bbf63310079a109d5eecb725e468bf14e9f765ce
henriqueacf <henrique.assis145@gmail.com>
2022-04-04 09:26:05 -0300

    feat [UserController]: adcionado yup para fazer avalidação dos dados

--- NPS/src/controller/UserController.ts
+++ NPS/src/controller/UserController.ts
@@ -1,12 +1,23 @@
 import {Request, Response} from "express";
 import {getCustomRepository} from "typeorm";
 import {UsersRepository} from "../repository/UsersRepository";
+import * as yup from 'yup'
 
 export class UserController{
     async create(req: Request, res:Response){
 
         const {name, email} = req.body
 
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup.string().email().required()
+        })
+
+        try{
+            await schema.validate(req.body, {abortEarly: false})
+        } catch(err){
+            return res.status(400).json({error: err})
+        }
         const usersRepository = getCustomRepository(UsersRepository)
 
         const userALreadyExists = await usersRepository.findOne({email})
