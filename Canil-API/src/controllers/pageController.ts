006d14261dc8ecd9c31db529a77d844dbd28a664
henriqueacf <henrique.assis145@gmail.com>
2022-04-05 09:03:03 -0300

    feat [Header]: Active Menu

--- Canil-API/src/controllers/pageController.ts
+++ Canil-API/src/controllers/pageController.ts
@@ -1,7 +1,9 @@
 import { Request, Response} from "express";
+import {createMenuObject} from "../helpers/createMenuObject";
 
 export const home = (req:Request, res: Response) =>{
     res.render('pages/page', {
+        menu: createMenuObject('all'),
         banner:{
             title:'Todos os animais',
             background:'allanimals.jpg'
@@ -11,17 +13,19 @@ export const home = (req:Request, res: Response) =>{
 
 export const dogs = (req:Request, res: Response) =>{
     res.render('pages/page', {
-        banner:{
-            title:'Todos os cachorros',
-            background:'banner_dog.jpg'
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
         }
     })
 }
 
 export const cats = (req:Request, res: Response) =>{
     res.render('pages/page', {
+        menu: createMenuObject('cat'),
         banner:{
-            title:'Todos os gatos',
+            title:'gatos',
             background:'banner_cat.jpg'
         }
     })
@@ -29,8 +33,9 @@ export const cats = (req:Request, res: Response) =>{
 
 export const fishes = (req:Request, res: Response) =>{
     res.render('pages/page', {
+        menu: createMenuObject('fish'),
         banner:{
-            title:'Todos os peixes',
+            title:'peixes',
             background:'banner_fish.jpg'
         }
     })
