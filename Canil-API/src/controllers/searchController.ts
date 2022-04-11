db34429ca432485910c9cb3e3d25ac719c43cffc
henriqueacf <henrique.assis145@gmail.com>
2022-04-11 09:18:51 -0300

    feat [searchController]: adcionando função de busca

--- Canil-API/src/controllers/searchController.ts
+++ Canil-API/src/controllers/searchController.ts
@@ -1,5 +1,19 @@
-import { Request, Response} from "express";
+import { Request, Response} from "express"
+import {pet} from '../models/pet'
+import {createMenuObject} from "../helpers/createMenuObject"
 
 export const search = (req:Request, res: Response) =>{
+    const query: string = req.query.q as string
+    const list = pet.getFromName(query)
 
+    if(!query){
+        res.redirect('/')
+        return
+    }
+
+    res.render('pages/page', {
+        menu: createMenuObject(''),
+        list,
+        query
+    })
 }
