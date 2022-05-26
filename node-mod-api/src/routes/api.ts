41dc72638814a456ba7583ba30a21af58f78ec24
henriqueacf <henrique.assis145@gmail.com>
2022-05-26 11:04:23 -0300

    up

--- node-mod-api/src/routes/api.ts
+++ node-mod-api/src/routes/api.ts
@@ -6,4 +6,14 @@ router.get('/ping', (req, res)=>{
     res.json({pong:true})
 })
 
+router.get('/random', (req, res) =>{
+    const nRand: number = Math.floor(Math.random() *10)
+    res.json({number: nRand})
+})
+
+router.get('/nome/:nome', (req, res)=>{
+    const nome: string = req.params.nome
+    res.json({nome})
+})
+
 export  default router
