ed26b1480fa6df438f4bc87dd063a149cdd22271
henriqueacf <henrique.assis145@gmail.com>
2022-05-27 15:32:51 -0300

    up crud

--- TODO/src/controllers/todo.controller.ts
+++ TODO/src/controllers/todo.controller.ts
@@ -1,17 +1,59 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express'
 import { Todo } from '../models/Todo'
 
-export const all = async() => {
-
+export const all = async(req: Request, res: Response) => {
+    const list = await Todo.findAll()
+    res.json({ list })
 }
 
-export const add = async ()=>{
+export const add = async (req: Request, res: Response)=>{
+    if(req.body.title){
+        const newToDo = await Todo.create({
+            title: req.body.title,
+            done: req.body.done ? true : false
+        })
+
+        res.status(201).json({item: newToDo})
+
+    } else {
+        res.json({
+            error: 'Dados não enviados.'
+        })
+    }
 
 }
- export  const update = async() =>{
+export const update = async(req: Request, res: Response) =>{
+    const id:string = req.params.id
 
- }
+    let todo = await Todo.findByPk(id)
+    if(todo) {
+        if(req.body.title) todo.title = req.body.title
 
- export const remove = async() =>{
+        if(req.body.done){
+            switch (req.body.done.toLowerCase()) {
+                case 'true':
+                case '1':
+                    todo.done = true
+                    break
+                case 'false':
+                case '2':
+                    todo.done = false
+            }
+        }
 
- }
+        await todo.save()
+        res.json({item: todo})
+
+    }else {
+        res.json({error: 'Item não encontrado'})
+    }
+}
+
+export const remove = async(req: Request, res: Response) =>{
+    const id:string = req.params.id
+
+    const todo = await Todo.findByPk(id)
+    if(todo) await todo.destroy()
+
+    res.status(200).json({Success: 'Item removido com sucesso'})
+}
