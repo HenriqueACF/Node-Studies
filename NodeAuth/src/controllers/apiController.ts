401aa97ac02f0e975fe999745034a52fd4c836c4
henriqueacf <henrique.assis145@gmail.com>
2022-05-31 17:23:05 -0300

    feat: [auth] -> gerando e validando token com JWT

--- NodeAuth/src/controllers/apiController.ts
+++ NodeAuth/src/controllers/apiController.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express';
 import { User } from '../models/User';
+import JWT from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 
 export const register = async (req: Request, res: Response) => {
@@ -10,8 +14,14 @@ export const register = async (req: Request, res: Response) => {
         if(!hasUser) {
             let newUser = await User.create({ email, password });
 
+            const token = JWT.sign(
+                {id: newUser.id, email: newUser.email},
+                process.env.JWT_SECRET_KEY as string,
+                { expiresIn: '1h'}
+            )
+
             res.status(201);
-            res.json({ id: newUser.id });
+            res.json({ id: newUser.id, token });
         } else {
             res.json({ error: 'E-mail já existe.' });
         }
@@ -30,7 +40,12 @@ export const login = async (req: Request, res: Response) => {
         });
 
         if(user) {
-            res.json({ status: true });
+            const token = JWT.sign(
+                {id: user.id, email: user.email},
+                process.env.JWT_SECRET_KEY as string,
+                { expiresIn: '1h'}
+            )
+            res.json({ status: true, token });
             return;
         }
     }
--- NodeAuth/src/middlewares/auth.ts
+++ NodeAuth/src/middlewares/auth.ts
@@ -1,26 +1,47 @@
 import { Request, Response, NextFunction } from 'express'
 import { User } from '../models/User'
+import JWT from 'jsonwebtoken'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export const Auth = {
     private: async(req: Request, res: Response, next: NextFunction) => {
         let success = false
-        //TODO -> fazer verificação do auth
+        //TODO -> fazer verificação do auth com basic auth
         console.log(req.headers.authorization)
 
+        //BASIC AUTH
+        // if(req.headers.authorization){
+        //     const hash: string = req.headers.authorization.substring(6)
+        //     const decoded: string = Buffer.from(hash, 'base64').toString()
+        //     const data: string[] = decoded.split(':')
+        //
+        //     if( data.length === 2 ){
+        //         const hasUser = await User.findOne({
+        //             where:{
+        //                 email: data[0],
+        //                 password: data[1]
+        //             }
+        //         })
+        //
+        //         if(hasUser) success = true
+        //     }
+        // }
+
+        //TODO -> fazer verificação do auth com JWT
         if(req.headers.authorization){
-            const hash: string = req.headers.authorization.substring(6)
-            const decoded: string = Buffer.from(hash, 'base64').toString()
-            const data: string[] = decoded.split(':')
+            const [authType, token] = req.headers.authorization.split('')
+            if(authType === 'Bearer'){
+                try{
+                    const decoded = JWT.verify(token, process.env.JWT_SECRET_KEY as string)
 
-            if( data.length === 2 ){
-                const hasUser = await User.findOne({
-                    where:{
-                        email: data[0],
-                        password: data[1]
-                    }
-                })
+                    console.log('decoded', decoded)
+                    success = true
+                } catch (err){
 
-                if(hasUser) success = true
+                }
+
             }
         }
 
