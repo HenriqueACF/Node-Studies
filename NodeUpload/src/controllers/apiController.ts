b27b6e4ae949bebfd590402a64b7bc736d4597e2
henriqueacf <henrique.assis145@gmail.com>
2022-05-31 10:58:09 -0300

    feat: [api] -> deletando imagens temporarias

--- NodeUpload/src/controllers/apiController.ts
+++ NodeUpload/src/controllers/apiController.ts
@@ -1,15 +1,20 @@
 import { Request, Response } from 'express';
 import sharp from 'sharp'
+import {unlink} from 'fs/promises'
 
 export const uploadFile = async ( req: Request, res: Response) => {
 
     if(req.file){
+        const fileName = `${req.file.filename}.jpg`
         await sharp(req.file.path)
             .resize(100)
             .toFormat('jpeg')
-            .toFile(`./public/media/${req.file.filename}.jpg`)
+            .toFile(`./public/media/${fileName}`)
 
-        res.send({image: `${req.file.filename}.jpg`})
+        //DELETANDO IMAGENS TEMPORARIAS
+        await  unlink(req.file.path)
+
+        res.send({image: `${fileName}.jpg`})
     } else {
         res.status(400)
         res.json({error: 'Arquivo inválido'})
